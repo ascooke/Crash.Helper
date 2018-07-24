6cd900f6ef43f4391c806697d63ec3b3070318bb
Mark Hatfield <grimelios@gmail.com>
2018-07-24 03:54:04 -0500

    Commit

---
+++ Crash.Helper/HelperForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Crash.Helper
+{
+    partial class HelperForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.foundLabel = new System.Windows.Forms.Label();
+			this.hotkeyLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// foundLabel
+			// 
+			this.foundLabel.AutoSize = true;
+			this.foundLabel.Location = new System.Drawing.Point(13, 13);
+			this.foundLabel.Name = "foundLabel";
+			this.foundLabel.Size = new System.Drawing.Size(99, 13);
+			this.foundLabel.TabIndex = 0;
+			this.foundLabel.Text = "[Address not found]";
+			// 
+			// hotkeyLabel
+			// 
+			this.hotkeyLabel.AutoSize = true;
+			this.hotkeyLabel.Location = new System.Drawing.Point(13, 30);
+			this.hotkeyLabel.Name = "hotkeyLabel";
+			this.hotkeyLabel.Size = new System.Drawing.Size(99, 13);
+			this.hotkeyLabel.TabIndex = 1;
+			this.hotkeyLabel.Text = "[Hotkey unpressed]";
+			// 
+			// HelperForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(294, 185);
+			this.Controls.Add(this.hotkeyLabel);
+			this.Controls.Add(this.foundLabel);
+			this.Name = "HelperForm";
+			this.Text = "Crash Helper";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.HelperForm_Load);
+			this.VisibleChanged += new System.EventHandler(this.HelperForm_VisibleChanged);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.Label foundLabel;
+		private System.Windows.Forms.Label hotkeyLabel;
+	}
+}
+
---
+++ Crash.Helper/HelperForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Crash.Helper.Memory;
+using Timer = System.Timers.Timer;
+
+namespace Crash.Helper
+{
+    public partial class HelperForm : Form
+	{
+		[Flags]
+		public enum Modifiers : uint
+		{
+			Alt = 1,
+			Control = 2,
+			Shift = 4,
+			Win = 8
+		}
+
+		// See https://stackoverflow.com/questions/2450373/set-global-hotkeys-using-c-sharp.
+		[DllImport("user32.dll")]
+		private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+		[DllImport("user32.dll")]
+		private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+		private HelperMemory memory;
+
+		public HelperForm()
+		{
+			InitializeComponent();
+	        RegisterHotKey(Handle, 0, (int)Modifiers.Alt, (uint)Keys.D.GetHashCode());
+		}
+
+		protected override void WndProc(ref Message m)
+		{
+			base.WndProc(ref m);
+
+			if (m.Msg != 0x0312)
+			{
+				return;
+			}
+
+			Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+			Modifiers modifier = (Modifiers)((int)m.LParam & 0xFFFF);
+			int id = m.WParam.ToInt32();
+
+			if (id == 0)
+			{
+				hotkeyLabel.Text = "[Hotkey pressed]";
+				memory.SetZeroLives();
+			}
+		}
+
+		private class KeyPressedEventArgs : EventArgs
+	    {
+		    public KeyPressedEventArgs(Modifiers modifier, Keys key)
+		    {
+			    Modifier = modifier;
+			    Key = key;
+		    }
+
+		    public Modifiers Modifier { get; }
+		    public Keys Key { get; }
+	    }
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			UnregisterHotKey(Handle, 0);
+		}
+
+		private void HelperForm_Load(object sender, EventArgs e)
+		{
+		}
+
+		private void HelperForm_VisibleChanged(object sender, EventArgs e)
+		{
+			memory = new HelperMemory();
+			memory.HookProcess();
+
+			if (memory.LivesFound)
+			{
+				int lives = memory.Lives;
+
+				foundLabel.Text = $"[Address found! ({lives} {(lives == 1 ? "life" : "lives")})]";
+			}
+		}
+	}
+}
